956aad362ad4fc02a79e9082c4d8a480ba3b8085
HamzaHandanagic <hamza_handan@hotmail.com>
2023-09-18 15:57:11 +0200

    Dealing with time

---
+++ TestingInCSharp/1.UnitTesting/4.DealingWithDateTime/DateTimeEdgeCases/IDateTimeProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeEdgeCases
+{
+    public interface IDateTimeProvider
+    {
+        public DateTimeOffset Now { get; }
+        public DateTimeOffset UtcNow { get; }
+    }
+
+    public class DateTimeProvider : IDateTimeProvider 
+    {
+        public DateTimeOffset Now => DateTimeOffset.Now;
+        public DateTimeOffset UtcNow => DateTimeOffset.UtcNow;
+    }
+}
