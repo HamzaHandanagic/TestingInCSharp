a50ae0ca547dbc6099b3643385ae074b723fae8d
HamzaHandanagic <hamza_handan@hotmail.com>
2023-09-19 14:09:42 +0200

    Code coverage

---
+++ TestingInCSharp/1.UnitTesting/5.CodeCoverage/StringChecker.Tests.Unit/StringCheckerTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace StringChecker.Tests.Unit
+{
+    public class StringCheckerTests
+    {
+        [Fact]
+        public void IsStringLong_ShouldReturnTrue_WhenStringIsLongerThan6Characters()
+        {
+            // Arrange
+            var sut = new StringChecker();
+            var input = "skafiskafnjak";
+
+            // Act
+            var result = sut.IsStringLong(input);
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
